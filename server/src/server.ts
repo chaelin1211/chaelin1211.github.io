065377ed3739a4f7b0f903ffbe40db0477a8aed0
chaelin1211 <cofls749@naver.com>
2023-12-23 18:15:43 +0900

    FEAT: notion database 조회 API Filter 기능 추가

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
-import express, { Request, Response } from "express";
-import {NotionPostResult, PostSimple, OriginPropsType, notionPostResultParse} from "./notionResult";
+import express, {Request, Response} from "express";
+import {NotionPostResult, PostSimple, notionPostResultParse} from "./notionResult";
+import {NotionPostAndFilter} from "./notionFilter";
+
 require("dotenv").config();
 const {Client} = require("@notionhq/client");
 
@@ -13,14 +15,27 @@ const notion = new Client({
 
 app.get("/post-list", async (req: Request, res: Response) => {
     const category = req.query.category as string | undefined || "";
-    const tags: string[] = req.query.tags as string[] | undefined || [];
+    const tags: string[] = Array.isArray(req.query.tags)
+        ? req.query.tags as string[] | undefined || []
+        : [req.query.tags as string | undefined || ""];
 
     // Avoid CORS errors
     // res.setHeader("Access-Control-Allow-Origin", "*");
     // res.setHeader("Content-Type", "application/json");
 
+    const notionAddFilter = new NotionPostAndFilter();
+    if (!category) {
+        notionAddFilter.addFilter("category", category);
+    }
+    if (tags.length > 0) {
+        tags.map((v: string) => {
+            notionAddFilter.addFilter("tags", v);
+        })
+    }
+
     const getPost: NotionPostResult = await notion.databases.query({
-        database_id: process.env.NOTION_DATABASE_ID
+        database_id: process.env.NOTION_DATABASE_ID,
+        filter: notionAddFilter
     })
 
     const postList: PostSimple[] = getPost.results.map((v) => {
