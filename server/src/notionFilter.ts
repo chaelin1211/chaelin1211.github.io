5a29a5bf0d5ea35be07e41729567707899006ed0
chaelin1211 <cofls749@naver.com>
2023-12-23 18:15:06 +0900

    FEAT: notion database filter를 위한 class, interface 추가

---
+++ server/src/notionFilter.ts
@@ -0,0 +1,25 @@
+interface MultiSelectFilter {
+    property: string,
+    "multi_select": {
+        contains: string
+    }
+}
+
+class NotionPostAndFilter {
+    and: MultiSelectFilter[]
+
+    constructor() {
+        this.and = [];
+    }
+
+    addFilter(property: string, contains: string) {
+        this.and.push({
+            property: property,
+            "multi_select": {
+                contains: contains
+            }
+        });
+    }
+}
+
+export {NotionPostAndFilter};
