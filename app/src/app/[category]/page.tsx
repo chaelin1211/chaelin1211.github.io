80e25243d51ed4e9a4d91f545cc5da89ca11d3d6
chaelin1211 <cofls749@naver.com>
2023-12-26 19:47:28 +0900

    FEAT: Category 별 게시글 목록 출력

--- app/src/app/[category]/page.tsx
+++ app/src/app/[category]/page.tsx
@@ -5,9 +5,28 @@ export async function generateStaticParams() {
     const props = await fetch("http://localhost:8000/properties").then((res) => res.json())
 
     // [{category: ""}, {category: ""}]
-    return props.category.map((v: {name: string}) => ({category: v.name}))
+    return props.category.map((v: { name: string }) => ({category: v.name}))
 }
 
-export default function Page(params: {params: { category: string }}) {
-    return <div>{params.params.category}</div>;
+const postList = async (category: string) => {
+    const url = new URL("http://localhost:8000/post-list");
+    url.searchParams.append("category", category)
+
+    return await fetch(url.toString()).then((res) => res.json())
+}
+
+export default async function Page(params: { params: { category: string } }) {
+    const category = params.params.category;
+    const posts = await postList(category);
+    console.log(posts)
+    return (
+        <div>
+            <h1>{category}</h1>
+            <ul>
+                {posts.map((post: {title: string}) => (
+                    // 반복되는 요소에는 고유한 key 속성을 제공해야 합니다.
+                    <li key={post.title}>{post.title}</li>
+                ))}
+            </ul>
+        </div>);
 }
