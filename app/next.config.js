57b1518e6d3293195f0e5605cca774c93bd028eb
chaelin1211 <cofls749@naver.com>
2023-12-27 19:02:20 +0900

    CHORE: 빌드 시, 환경변수 사용을 위한 설정

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
-}
+    output: 'export',
+    env: {
+        NOTION_SECRET: process.env.NOTION_SECRET,
+        NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID
+    },
 
+}
 module.exports = nextConfig
