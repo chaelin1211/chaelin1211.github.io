b959d7c2344c37191b40e8d0fd81475eeb70b574
chaelin1211 <cofls749@naver.com>
2023-12-27 19:25:18 +0900

    CHORE: Vercel 배포로 변경

--- app/next.config.js
+++ app/next.config.js
@@ -1,10 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    env: {
-        NOTION_SECRET: process.env.NOTION_SECRET,
-        NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID
-    },
-
-}
+const nextConfig = {}
 module.exports = nextConfig
