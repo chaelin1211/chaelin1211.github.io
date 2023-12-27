16d2fc1f467a34defb5c9961969f31936f8f517b
chaelin01211 <chaelin01211@mobigen.com>
2023-12-28 00:42:28 +0900

    FIX: CSR 사용 부분 useEffect 사용해서 변경

--- app/src/app/[category]/post-list.tsx
+++ app/src/app/[category]/post-list.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+'use client';
+import React, {useEffect, useState} from 'react';
 import {getPostList} from "@/src/app/notion/notion-service";
+import {PostSimple} from "@/src/app/notion/notion-result";
 
 const PostList: React.FC<{ category: string }> = async ({category}) => {
-    const postList = await getPostList(category);
+    const [postList, setPostList] = useState<PostSimple[]>([]);
+    useEffect(() => {
+        const fetchPostList = async () => {
+            const postList = await getPostList(category);
+            setPostList(postList);
+        }
+        fetchPostList();
+    }, [category]);
+
     return (
         <ul>
             {postList.map((item, index) => (
