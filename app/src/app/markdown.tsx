78c6ec59ef48f17cdbbf6c510e82d0ed0a809dbd
chaelin1211 <cofls749@naver.com>
2023-12-28 19:35:06 +0900

    FEAT: md -> html로 화면에 띄우기 위한 공통 컴포넌트 생성

---
+++ app/src/app/markdown.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import remarkBreaks from "remark-breaks";
+import remarkParse from "remark-parse";
+import remarkStringify from "remark-stringify"; // 추가된 부분
+
+interface MarkdownComponentProps {
+  markdown: string;
+}
+
+const MarkdownComponent: React.FC<MarkdownComponentProps> = ({ markdown }) => {
+  return (
+    <div>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkBreaks, remarkParse, remarkStringify]}
+      >
+        {markdown}
+      </ReactMarkdown>
+    </div>
+  );
+};
+
+export default MarkdownComponent;
