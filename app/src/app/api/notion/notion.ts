4c6fc0862c9aab64da8ac118a4996e1377c2ede8
chaelin1211 <cofls749@naver.com>
2023-12-28 19:32:15 +0900

    FEAT: Notion page 내용 MD 형태로 가져오는 라이브러리 추가

--- app/src/app/api/notion/notion.ts
+++ app/src/app/api/notion/notion.ts
@@ -1,6 +1,9 @@
 const { Client } = require("@notionhq/client");
+const { NotionToMarkdown } = require("notion-to-md");
 
 // Initializing a client
 export const NotionClient = new Client({
   auth: process.env.NOTION_SECRET,
 });
+
+export const NotionToMd = new NotionToMarkdown({ notionClient: NotionClient });
